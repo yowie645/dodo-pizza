6430530cf4cd05e9049c586676d547834e05e374
minxrka <minorka04@mail.ru>
2024-12-08 18:39:06 +0300

    * schema.prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Container, Filters, Title, TopBar } from "@/components/fragments";
+import { ProductsGroupList } from "@/components/fragments/products-group-list";
 
 export default function Home() {
   return (
@@ -14,7 +15,116 @@ export default function Home() {
             <Filters />
           </div>
           <div className="flex-1">
-            <div className="flex flex-col gap-16">список</div>
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title={"Пиццы"}
+                items={[
+                  {
+                    id: 1,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={1}
+              />
+              <ProductsGroupList
+                title={"Комбо"}
+                items={[
+                  {
+                    id: 1,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: "Маргарита",
+                    price: 1500,
+                    imageUrl:
+                      "https://media.dodostatic.net/image/r:292x292/11EE7D6105EF6690B86FBDE6150B5B0C.avif",
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={2}
+              />
+            </div>
           </div>
           {/* product list */}
         </div>
--- components/fragments/categories.tsx
+++ components/fragments/categories.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { cn } from "@/lib/utils";
+import { useCategoryStore } from "@/store/category";
 import React from "react";
 
 interface Props {
@@ -6,32 +9,38 @@ interface Props {
 }
 
 const cats = [
-  "Пиццы",
-  "Комбо",
-  "Закуски",
-  "Коктейли",
-  "Кофе",
-  "Напитки",
-  "Десерты",
-  "Соусы",
+  { id: 1, name: "Пиццы" },
+  { id: 2, name: "Комбо" },
+  { id: 3, name: "Закуски" },
+  { id: 4, name: "Завтрак" },
+  { id: 5, name: "Коктейли" },
+  { id: 6, name: "Кофе" },
+  { id: 7, name: "Напитки" },
+  /*   { id: 8, name: "Десерты" }, */
+  /*   { id: 9, name: "Любят дети" }, */
+  { id: 10, name: "Соусы" },
+  /*   { id: 11, name: "Другие товары" },
+  { id: 12, name: "Акции" }, */
 ];
-const activeIndex = 0;
 
 export const Categories: React.FC<Props> = ({ className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
+
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {cats.map((cat, index) => (
+      {cats.map(({ name, id }, index) => (
         <a
           className={cn(
             "flex items-center font-medium h-11 rounded-2xl px-5",
-            activeIndex === index &&
+            categoryActiveId === id &&
               "bg-white shadow-md shadow-gray-200 text-primary"
           )}
           key={index}
+          href={`/#${name}`}
         >
-          <button>{cat}</button>
+          <button>{name}</button>
         </a>
       ))}
     </div>
