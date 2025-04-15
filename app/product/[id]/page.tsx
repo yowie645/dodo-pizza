a010b28e6b5c1ce48b8889c6027bab49928f96b0
minxrka <minorka04@mail.ru>
2025-04-15 23:44:09 +0300

    *page *product-image

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@/components/fragments';
+import { Container, ProductImage, Title } from '@/components/fragments';
 import { prisma } from '@/prisma/prisma-client';
 import { notFound } from 'next/navigation';
 
@@ -12,10 +12,26 @@ export default async function ProductPage({
   if (!product) return notFound();
   return (
     <Container className='flex flex-col my-10'>
-      <ProductImage
-        src={product.imageUrl}
-        className=''
-      />
+      <div className='flex flex-1'>
+        <ProductImage
+          imageUrl={product.imageUrl}
+          size={40}
+          className=''
+        />
+        <div className='w-[490px] bg-[#fcfcfc] p-7'>
+          <Title
+            text={product.name}
+            size='md'
+            className='font-extrabold mb-1'
+          />
+          <p className='text-gray-400'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+            nobis provident culpa veritatis architecto. Voluptate est molestiae
+            ex, dolorum sint sed beatae, in iusto natus error quam, repellendus
+            maiores dolores!
+          </p>
+        </div>
+      </div>
     </Container>
   );
 }
