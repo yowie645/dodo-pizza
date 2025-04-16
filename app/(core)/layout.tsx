8084eb52e92781e3e4320c5464394b4c971bdba7
minxrka <minorka04@mail.ru>
2025-04-16 23:52:41 +0300

    *layout

--- app/(core)/layout.tsx
+++ app/(core)/layout.tsx
@@ -1,30 +1,21 @@
 import type { Metadata } from 'next';
-import { Comfortaa } from 'next/font/google';
-import '../globals.css';
-import { Header } from '@/components/fragments/header';
 
-const comfortaa = Comfortaa({
-  subsets: ['cyrillic'],
-  variable: '--font-nunito',
-  weight: ['400', '500', '600', '700'],
-});
+import { Header } from '@/components/fragments/header';
 
 export const metadata: Metadata = {
   title:
     'Пицца — заказать с доставкой на дом бесплатно, доставка еды и напитков', //todo сделать с привязкой к определенному городу Пицца МСК и т.д
 };
 
-export default function RootLayout({
+export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={comfortaa.variable}>
-        <Header />
-        <main className='min-h-screen'>{children}</main>
-      </body>
-    </html>
+    <main className='min-h-screen'>
+      <Header />
+      {children}
+    </main>
   );
 }
