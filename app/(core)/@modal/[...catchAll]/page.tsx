5d2bf617fcd8120f87b0c51f0642e84e304b7054
minxrka <minorka04@mail.ru>
2025-04-17 21:23:59 +0300

    *default *layout -route +page fix

--- app/(core)/@modal/[...catchAll]/page.tsx
+++ app/(core)/@modal/[...catchAll]/page.tsx
@@ -1,3 +1,3 @@
-export default function DefaultModal() {
+export default function CatchAll() {
   return null;
 }
--- app/(core)/@modal/default.tsx
+++ app/(core)/@modal/default.tsx
@@ -1,3 +1,3 @@
-export default function DefaultModal() {
+export default function Default() {
   return null;
 }
