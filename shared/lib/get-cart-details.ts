e40865489ada0b75d896687749de850371fa986d
minxrka <minorka04@mail.ru>
2025-04-26 22:15:47 +0300

    +cart-dto *get-cart-details

--- shared/lib/get-cart-details.ts
+++ shared/lib/get-cart-details.ts
@@ -1,1 +1,11 @@
-export const getCartDetails = (data: any[]) => {};
+import { Cart } from '@prisma/client';
+import { CartState } from '../store/cart';
+
+interface ReturnProps {
+  items: CartState[];
+  totalAmount: number;
+}
+
+export const getCartDetails = (data: Cart) => {
+  return;
+};
